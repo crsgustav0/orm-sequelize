04b16ad56653942cddab77b3af20d0912dbb95df
crsgustav0 <cristian.dias@terraviva.agr.br>
2021-08-10 20:36:00 -0300

    style: Indentaçao arquivo 'pessoas.js'

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -3,36 +3,35 @@ module.exports = (sequelize, DataTypes) => {
     const Pessoas = sequelize.define(
         "Pessoas", {
             /*Passa via objeto as especificações 
-            da criação do objeto*/
+                  da criação do objeto*/
             nome: {
                 dataTypes: DataTypes.STRING,
                 /*Passa via objeto as validações,
-                 função criada para validar o campo onde, 
-                 em caso de erro retorna uma mensagem*/
+                         função criada para validar o campo onde, 
+                         em caso de erro retorna uma mensagem*/
                 validate: {
                     validarNomePessoa: function(dados) {
-                        if (dados.length < 3) throw new Error('O campo nome deve ter mais de 3 caracteres')
-
-                    }
-                }
+                        if (dados.length < 3)
+                            throw new Error("O campo nome deve ter mais de 3 caracteres");
+                    },
+                },
             },
             ativo: DataTypes.BOOLEAN,
             /*Passa via objeto as especificações 
-            da criação do objeto*/
+                  da criação do objeto*/
             email: {
                 dataType: DataTypes.STRING,
                 /*Passa via objeto as 
-                validações do campo*/
+                        validações do campo*/
                 validate: {
                     /*Passa via objeto as 
-                    especificações utilizadas 
-                    para validaçõões do campo*/
+                              especificações utilizadas 
+                              para validaçõões do campo*/
                     isEmail: {
                         args: true,
-                        msg: 'Dados tipo e-mail inválidos'
+                        msg: "Dados tipo e-mail inválidos",
                     },
-
-                }
+                },
             },
             nome: DataTypes.STRING,
         }, {
@@ -40,24 +39,23 @@ module.exports = (sequelize, DataTypes) => {
             /*Definiçao de escopo padrão*/
             defaultScope: {
                 /*Passando via parâmetro o campo que será 
-                usado como referễncia para consultar registros*/
+                        usado como referễncia para consultar registros*/
             },
 
             /*Definiçao de escopo alternativo*/
             scopes: {
                 /*Passando via parâmetro o campo que será 
-                  usado como referễncia para consultar registros*/
+                          usado como referễncia para consultar registros*/
                 todos: {
-                    where: {}
-                }
-            }
+                    where: {},
+                },
+            },
         }
     );
     /*Estabele uma conexão entre bancos, relacionamento */
     Pessoas.associate = function(models) {
         Pessoas.hasMany(models.Turmas, {
             /*Nome escolhido para criação associação entre tabelas*/
-
             foreignKey: "docente_id",
         });
         /*Estabele uma conexão entre bancos, relacionamento */
