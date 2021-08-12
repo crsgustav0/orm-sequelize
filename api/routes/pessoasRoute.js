56aa04fe482663009f95b65612d541d9999a2cf8
crsgustav0 <cristian.dias@terraviva.agr.br>
2021-08-11 22:06:56 -0300

    style: Indentaçao arquivo 'pessoasRoute.js'

--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -6,10 +6,10 @@ importado através da Constante 'PessoaController' */
 
 router
 
-//Requisição GET, busca todos os registros ativos do banco
+/*Requisição GET, busca todos os registros ativos do banco*/
     .get("/pessoas/todos", PessoasControler.buscarPessoas)
 
-//Requisição GET, busca todos os registros ativos do banco
+/*Requisição GET, busca todos os registros ativos do banco*/
 .get("/pessoas", PessoasControler.buscarPessoasAtivas)
 
 /*Requisição GET, busca de registros do banco 
@@ -18,54 +18,36 @@ passando o id como parametro ':id' */
 
 /*Requisiçao GET, busca de registro do banco 
 passando o ID como parâmetro ':id'  */
-.get('pessoas/:estudanteId/matricula', PessoasControler.buscarMatriculas)
+.get("pessoas/:estudanteId/matricula", PessoasControler.buscarMatriculas)
 
 /*Requisição GET, busca de registros do banco 
 passando o id como parametro ':id' e o método a 
 ser executado na busca de registros*/
-.get(
-    "/pessoas/:estudanteId/matricula/:id",
-    PessoasControler.buscarMatriculaPessoasByID
-)
+.get("/pessoas/:estudanteId/matricula/:id", PessoasControler.buscarMatriculaPessoasByID)
 
 /*Requisição GET, busca de registros do banco 
 passando o id como parametro ':id' e o método a 
 ser executado na busca de registros*/
-.get(
-    "/pessoas/:matricula/:turmasId/confirmadas",
-    PessoasControler.buscarMatriculasByTurma
-)
+.get("/pessoas/:matricula/:turmasId/confirmadas", PessoasControler.buscarMatriculasByTurma)
 
 /*Requisição GET, busca de registros do banco 
 passando o id como parametro ':id' e o método a 
 ser executado na busca de registros*/
-.get(
-    "/pessoas/:matricula/lotada",
-    PessoasControler.buscarTurmasLotadas
-)
+.get("/pessoas/:matricula/lotada", PessoasControler.buscarTurmasLotadas)
 
 /*Requisição put, gravação de registros do banco 
 passando o id como parametro ':id' */
-.get(
-    "/pessoas/:estudanteId/matricula/:id",
-    PessoasControler.atualizarMatriculaByID
-)
+.get("/pessoas/:estudanteId/matricula/:id", PessoasControler.atualizarMatriculaByID)
 
 /*Requisição put, gravação de registros do banco 
 passando o id como parametro ':id' */
-.get(
-    "/pessoas/:estudanteId/matricula/:id",
-    PessoasControler.deletarMatriculaByID
-)
+.get("/pessoas/:estudanteId/matricula/:id", PessoasControler.deletarMatriculaByID)
 
 //Requisição POST, gravação de registros no banco
 .post("/pessoas", PessoasControler.criarPessoas)
 
 //Requisição POST, gravação de registros no banco
-.post(
-    "/pessoas/:estudanteID/matricula/:matriculaId",
-    PessoasControler.criarMatricula
-)
+.post("/pessoas/:estudanteID/matricula/:matriculaId", PessoasControler.criarMatricula)
 
 /*Requisição post, restauração de registros no banco 
 passando o id como parametro ':id' */
@@ -73,7 +55,7 @@ passando o id como parametro ':id' */
 
 /*Requisição POST, deleção de registros relacionados entre 
 as tabelas Pessoas e Matriculas passando o ID como parametro ':id'*/
-.post('/pessoas/estudanteId/cancela', PessoasControler.cancelarPessoa)
+.post("/pessoas/estudanteId/cancela", PessoasControler.cancelarPessoa)
 
 /*Requisição POST, atualização de registros no banco
 passando o ID como parametro ':id' */
