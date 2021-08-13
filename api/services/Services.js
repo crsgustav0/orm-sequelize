90a70473f377b61d94fb18712759d38da2543272
crsgustav0 <cristian.dias@terraviva.agr.br>
2021-08-12 22:37:59 -0300

    feat: Criaçao serviços Matriculas, arquivo 'MatriculasControllers.js'

--- api/services/Services.js
+++ api/services/Services.js
@@ -1,4 +1,5 @@
 /*Classe responsável pela comunicação dos modelos e operações*/
+const { where } = require('sequelize/types')
 const database = require('../models')
 
 class Services {
@@ -6,18 +7,29 @@ class Services {
         this.nomeModelo = nomeModelo
     }
 
-    /*Usado na refatoração dos controladores onde, centralizando a execução de o
-    operações através da pasagem do seu modelo via parâmetro */
+    /*Usado na refatoração dos controladores onde, centralizando a execução de
+    operações através da pasagem do seu modelo via parâmetro,
+     */
     async buscarRegistros() {
-        return database[this.nomeModelo].findAll()
+        return database[this.nomeModelo].findAll({ where: {...where } })
     }
 
+    /*Usado na refatoração dos controladores onde, centralizada a execução de 
+    operações através do seu modelo via parâmetro e filtro de registros, 
+    sendo passados ou não*/
     async buscarRegistro(id) {
         return database[this.nomeModelo]
+            .findOne({ where: {...where } })
     }
 
-    async criarRegistro(dados) {
+    /*Usado na  */
+    async contarRegistros(
+        /*Passa via parâmetro a regra de pesquisa de registros */
+        where = {}, agregadores) {
         return database[this.nomeModelo]
+            .findAndCountAll(
+                /**/
+                { where: {...where }, ...agregadores })
     }
 
     async atualizarRegistro(dadosAtualizar, id) {
